87edc9d77637653ac19d6f8bcae796fa10b043b2
nightwing098 <86614628+nightwing098@users.noreply.github.com>
2023-12-04 00:51:35 +0530

    Update Firebase.tsx

--- config/Firebase.tsx
+++ config/Firebase.tsx
@@ -5,13 +5,7 @@ import "firebase/auth";
 require('firebase/firestore')
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD27EHQuNkdUNVn8Su9EHPz-aIOodWSsOA",
-  authDomain: "insta-clone-app-e1093.firebaseapp.com",
-  projectId: "insta-clone-app-e1093",
-  storageBucket: "insta-clone-app-e1093.appspot.com",
-  messagingSenderId: "222843265304",
-  appId: "1:222843265304:web:29f0c4242e7c8673ee3e6d",
-  measurementId: "G-9WCDV1H26Q"
+
 };
   // Initialize Firebase
 
